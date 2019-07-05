1edf3b62f316d66f52c7eb701328088685f1eaf6
James Swatton <jamessswatton@gmail.com>
2019-07-05 11:59:31 +0100

    added table tests

---
+++ src/test/java/com/codeclan/example/Restaurant/Booking/System/models/BookingTest.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Restaurant.Booking.System.models;
+
+import org.junit.Before;
+
+public class BookingTest {
+
+    Booking booking;
+
+    @Before
+    public void before(){
+        booking = new Booking()
+    }
+}
