39227227b7d3227c5b8885d6136a3f6e7274e513
Joseph Young <jsyoung0891@gmail.com>
2019-07-05 14:33:55 +0100

    made a couple of wonderful queries :)

--- src/main/java/com/codeclan/example/Restaurant/Booking/System/repositories/BookingRepository.java
+++ src/main/java/com/codeclan/example/Restaurant/Booking/System/repositories/BookingRepository.java
@@ -4,4 +4,5 @@ import com.codeclan.example.Restaurant.Booking.System.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
 }
--- src/main/java/com/codeclan/example/Restaurant/Booking/System/repositories/CustomerRepository.java
+++ src/main/java/com/codeclan/example/Restaurant/Booking/System/repositories/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.codeclan.example.Restaurant.Booking.System.repositories;
 import com.codeclan.example.Restaurant.Booking.System.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+import java.util.List;
 
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findAllCustomersByName(String name);
+    List<Customer> findAllCustomersByBookingCounter(int bookingCounter);
 }
