25578272dcc43a7a8ed073f0b3fb7d076803f096
James Swatton <jamessswatton@gmail.com>
2019-07-05 12:21:32 +0100

    added Customer tests

--- src/test/java/com/codeclan/example/Restaurant/Booking/System/models/CustomerTest.java
+++ src/test/java/com/codeclan/example/Restaurant/Booking/System/models/CustomerTest.java
@@ -1,4 +1,26 @@
 package com.codeclan.example.Restaurant.Booking.System.models;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
+    Customer customer;
+
+    @Before
+    public void before() {
+        customer = new Customer("Mr Blobby", "07888 888 888");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Mr Blobby", customer.getName());
+    }
+
+    @Test
+    public void hasPhoneNumber() {
+        assertEquals("07888 888 888", customer.getPhoneNumber());
+    }
 }
