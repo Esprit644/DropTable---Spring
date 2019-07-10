af68a964e1ec7147c3d756787fbfac3c020e1829
James Swatton <jamessswatton@gmail.com>
2019-07-10 21:07:42 +0100

    href added to booking class

--- src/main/java/com/codeclan/example/Restaurant/Booking/System/repositories/BookingRepository.java
+++ src/main/java/com/codeclan/example/Restaurant/Booking/System/repositories/BookingRepository.java
@@ -18,4 +18,5 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findBookingByCustomer_Name(String name);
     List<Booking> findBookingDateByCustomer_Name(String name);
     List<Booking> findBookingsByDiningTableIdAndDate(Long id, String date);
+
 }
